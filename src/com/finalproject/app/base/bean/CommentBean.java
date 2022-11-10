b2f6bc3e9953ed743871d9f5f5dabfd87ca9ac17
ShimShim27 <84265457+ShimShim27@users.noreply.github.com>
2022-11-10 11:48:56 +0800

    Removed  unrelated method

--- src/com/finalproject/app/base/bean/CommentBean.java
+++ src/com/finalproject/app/base/bean/CommentBean.java
@@ -3,5 +3,4 @@ package com.finalproject.app.base.bean;
 public interface CommentBean {
 	public String getCommentString();
 	public String getCommentStringPreview();
-	public String getUsername();
 }

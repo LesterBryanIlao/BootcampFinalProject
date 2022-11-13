e12c0da6543d33c7584b4a2513754b5484563f8d
ShimShim27 <84265457+ShimShim27@users.noreply.github.com>
2022-11-13 12:18:02 +0800

    Added getters, setters and constructor params

--- src/com/finalproject/app/entity/Post.java
+++ src/com/finalproject/app/entity/Post.java
@@ -8,4 +8,45 @@ public class Post {
 	private long upvotes;
 	private long time;
 
+	public Post(long id, long userId, String content, long upvotes, long time) {
+		super();
+		this.id = id;
+		this.userId = userId;
+		this.content = content;
+		this.upvotes = upvotes;
+		this.time = time;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public long getUserId() {
+		return userId;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public long getUpvotes() {
+		return upvotes;
+	}
+
+	public void setUpvotes(long upvotes) {
+		this.upvotes = upvotes;
+	}
+
+	public long getTime() {
+		return time;
+	}
+
+	public void setTime(long time) {
+		this.time = time;
+	}
+
 }
--- src/com/finalproject/app/entity/User.java
+++ src/com/finalproject/app/entity/User.java
@@ -1,11 +1,55 @@
 package com.finalproject.app.entity;
 
 public class User {
-	
+
 	private long id;
 	private String userName;
 	private String firstName;
 	private String lastName;
 	private String password;
+
+	public User(long id, String userName, String firstName, String lastName, String password) {
+		this.id = id;
+		this.userName = userName;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.password = password;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
 }

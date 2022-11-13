e57b32c8f0e3a0917fe358b4f07cf87e67318b73
ShimShim27 <84265457+ShimShim27@users.noreply.github.com>
2022-11-13 16:55:57 +0800

    Changed import and method params

--- src/main/java/com/finalproject/app/base/service/UserSessionManagementService.java
+++ src/main/java/com/finalproject/app/base/service/UserSessionManagementService.java
@@ -1,15 +1,19 @@
 package com.finalproject.app.base.service;
 
 import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import com.finalproject.app.base.repository.UserRepository;
 import com.finalproject.app.entity.LoginSession;
 
 public interface UserSessionManagementService {
-	public LoginSession loginUserViaEmailAndPassword(UserRepository userRepository, HttpServlet request, String email, String password);
-	public LoginSession loginUserViaExistingSession(UserRepository userRepository, HttpServlet request);
-	public boolean isLoggedIn(UserRepository userRepository, HttpSession session, Cookie [] cookies);
-	public void logoutUser(LoginSession loginSession) ;
+	public LoginSession loginUserViaEmailAndPassword(UserRepository userRepository, HttpServletRequest request, String email,
+			String password);
+
+	public LoginSession loginUserViaExistingSession(UserRepository userRepository, HttpServletRequest request);
+
+	public boolean isLoggedIn(UserRepository userRepository, HttpSession session, Cookie[] cookies);
+
+	public void logoutUser(HttpServletRequest request);
 }

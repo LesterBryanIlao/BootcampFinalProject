cf27d9e2cd386808e9289cc1f0f2dfb2a97d3e15
C0rvs <cyrus080600@gmail.com>
2022-11-15 16:52:43 +0800

    migrated PostRepository to hibernate
    migrated PostRepository to hibernate

--- src/main/java/app/entity/Post.java
+++ src/main/java/app/entity/Post.java
@@ -1,6 +1,7 @@
 package app.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 import lombok.Data;
 
@@ -8,51 +9,13 @@ import lombok.Data;
 @Data
 public class Post {
 
+    @Id
 	private long id;
 	private long userId;
 	private String content;
 	private long upvotes;
 	private long time;
 
-	public Post(long id, long userId, String content, long upvotes, long time) {
-		super();
-		this.id = id;
-		this.userId = userId;
-		this.content = content;
-		this.upvotes = upvotes;
-		this.time = time;
-	}
-
-	public long getId() {
-		return id;
-	}
-
-	public long getUserId() {
-		return userId;
-	}
-
-	public String getContent() {
-		return content;
-	}
-
-	public void setContent(String content) {
-		this.content = content;
-	}
-
-	public long getUpvotes() {
-		return upvotes;
-	}
-
-	public void setUpvotes(long upvotes) {
-		this.upvotes = upvotes;
-	}
-
-	public long getTime() {
-		return time;
-	}
-
-	public void setTime(long time) {
-		this.time = time;
-	}
+	
 
 }

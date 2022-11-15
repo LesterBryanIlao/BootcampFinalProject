967a328b41a8fc256af7eeae0f0a40a95e0dffff
ShimShim27 <84265457+ShimShim27@users.noreply.github.com>
2022-11-15 17:03:04 +0800

    Implemented java persistence

--- src/main/java/app/entity/Post.java
+++ src/main/java/app/entity/Post.java
@@ -1,58 +1,43 @@
 package app.entity;
 
+import java.io.Serializable;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 import lombok.Data;
 
 @Entity
+@Table(name = "Posts")
 @Data
-public class Post {
+public class Post implements Serializable{
 
+	private static final long serialVersionUID = 9172197894142144897L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="post_id", columnDefinition = "INT", nullable = false)
 	private long id;
-	private long userId;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id")
+	private User user;
+
+	@Column(name = "post_content", columnDefinition = "VARCHAR(500)")
 	private String content;
+
+	@Column(name = "up_votes", columnDefinition = "INT", nullable = false)
 	private long upvotes;
+
+	@Column(name = "date_posted", columnDefinition = "DATE", nullable = false)
 	private long time;
-
-	public Post(long id, long userId, String content, long upvotes, long time) {
-		super();
-		this.id = id;
-		this.userId = userId;
-		this.content = content;
-		this.upvotes = upvotes;
-		this.time = time;
-	}
-
-	public long getId() {
-		return id;
-	}
-
-	public long getUserId() {
-		return userId;
-	}
-
-	public String getContent() {
-		return content;
-	}
-
-	public void setContent(String content) {
-		this.content = content;
-	}
-
-	public long getUpvotes() {
-		return upvotes;
-	}
-
-	public void setUpvotes(long upvotes) {
-		this.upvotes = upvotes;
-	}
-
-	public long getTime() {
-		return time;
-	}
-
-	public void setTime(long time) {
-		this.time = time;
-	}
 
 }

a989904aeae6238e32e77faffee1d1807abe1af9
ShimShim27 <84265457+ShimShim27@users.noreply.github.com>
2022-11-16 20:33:48 +0800

    Update configuration files

--- src/main/java/app/controller/CommentController.java
+++ src/main/java/app/controller/CommentController.java
@@ -18,33 +18,37 @@ import javassist.expr.NewArray;
 @Controller
 @RequestMapping("/comments")
 public class CommentController {
-    
-   /* @Autowired
-    private UserCommentService userCommenetService;
-    
-    */
-    
-    @RequestMapping(method = RequestMethod.GET)
-    public ModelAndView showForm(ModelMap map) {
-      /*map.addAttribute("commentForm", new CommentForm());
-        map.addAttribute("comments", userCommentService.getAllCommentsInPost());
-        */
-        return new ModelAndView("commentForm");
-        
-    }
-    
-    public String submitCommentFormString(@Valid @ModelAttribute("commentForm") CommentForm commentForm, BindingResult bindingResiltBindingResult, Model model)
-    if(bindingResiltBindingResult.hasErrors()) {
-        return "commentForm";
-    }
-    try {
-        userCommentService.newComment(commentForm.getUserId(), commentForm.getCommentContent());
-    }catch (Exception e) {
-        model.addAttribute("useError", e.getMessage());
-        return "commentForm";
-        
-    }
-    
-    return "redirect:comments";
 
+	/*
+	 * @Autowired private UserCommentService userCommenetService;
+	 * 
+	 */
+
+	@RequestMapping(method = RequestMethod.GET)
+	public ModelAndView showForm(ModelMap map) {
+		/*
+		 * map.addAttribute("commentForm", new CommentForm());
+		 * map.addAttribute("comments", userCommentService.getAllCommentsInPost());
+		 */
+		return new ModelAndView("commentForm");
+
+	}
+
+	public String submitCommentFormString(@Valid @ModelAttribute("commentForm") CommentForm commentForm,
+			BindingResult bindingResiltBindingResult, Model model) {
+		if (bindingResiltBindingResult.hasErrors()) {
+
+			return "commentForm";
+		}
+
+		try {
+//        userCommentService.newComment(commentForm.getUserId(), commentForm.getCommentContent());
+		} catch (Exception e) {
+			model.addAttribute("useError", e.getMessage());
+			return "commentForm";
+
+		}
+
+		return "redirect:comments";
+	}
 }
--- src/main/java/app/repository/UserRepository.java
+++ src/main/java/app/repository/UserRepository.java
@@ -1,11 +1,13 @@
 package app.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import app.entity.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-	public User getViaEmailAndPassword(String email, String password);
+	
 }

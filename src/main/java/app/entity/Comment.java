f2ac6734f8df5ca7cd5d440fcf487b8ef221ba9f
definitelynotnes <nestorjohntenorio@gmail.com>
2022-11-16 12:51:01 +0800

    updated comment entity
    Removed unused libraries and whitespaces

--- src/main/java/app/entity/Comment.java
+++ src/main/java/app/entity/Comment.java
@@ -1,7 +1,7 @@
 package app.entity;
 
 import java.io.Serializable;
-import java.util.List;
+import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -32,13 +32,7 @@ public class Comment implements Serializable{
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "custom_comment_sequence")
     @SequenceGenerator(sequenceName = "comment_sequence", name = "custom_comment_sequence", allocationSize = 1)
-    /*
-    CREATE SEQUENCE comment_sequence
-    MINVALUE 1
-    MAXVALUE 9999999999
-    START WITH 1
-    INCREMENT BY 1;
-    */
+
     private long id;
     
     @ManyToOne(fetch = FetchType.LAZY)
@@ -53,6 +47,6 @@ public class Comment implements Serializable{
 	private String content;
 	
 	@Column(name = "date_posted", columnDefinition = "DATE", nullable = false)
-	private long time;
+	private Date time;
 
 }

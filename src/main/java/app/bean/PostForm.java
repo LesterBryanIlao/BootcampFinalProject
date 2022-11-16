5ec7dd1b88be27f0b38961525ed919dd8038225e
ShimShim27 <84265457+ShimShim27@users.noreply.github.com>
2022-11-16 11:27:16 +0800

    Added validation

--- src/main/java/app/bean/PostForm.java
+++ src/main/java/app/bean/PostForm.java
@@ -1,8 +1,12 @@
 package app.bean;
 
+import org.hibernate.validator.constraints.NotEmpty;
+
 import lombok.Data;
 
 @Data
 public class PostForm {
+	
+	@NotEmpty(message = "Please write something")
 	private String content;
 }

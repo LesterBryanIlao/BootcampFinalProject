1de10418ea979566445bbdd41941a20c6300ecd7
ShimShim27 <84265457+ShimShim27@users.noreply.github.com>
2022-11-20 21:31:58 +0800

    Added wrong ErrorControllerTest

--- src/test/java/app/suite/AllTestSuite.java
+++ src/test/java/app/suite/AllTestSuite.java
@@ -4,7 +4,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
-import app.controller.ErrorController;
+import app.controller.ErrorControllerTest;
 import app.controller.HomeControllerTest;
 import app.controller.PostControllerTest;
 import app.controller.PostFormControllerTest;
@@ -14,7 +14,7 @@ import app.util.sorter.ContentSorterTest;
 
 @RunWith(Suite.class)
 @SuiteClasses({ HomeControllerTest.class, PostFormControllerTest.class, CommentServiceImplTest.class,
-		PostServiceImplTest.class, ContentSorterTest.class, PostControllerTest.class, ErrorController.class })
+		PostServiceImplTest.class, ContentSorterTest.class, PostControllerTest.class, ErrorControllerTest.class })
 public class AllTestSuite {
 
 }

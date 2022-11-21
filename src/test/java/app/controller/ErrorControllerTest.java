ae962f8bd00b04770fd4fb5ec5344c35f6b47fd1
LesterBryanIlao <lestertetengilao@gmail.com>
2022-11-21 10:35:22 +0800

    descending order of comment

--- src/test/java/app/controller/ErrorControllerTest.java
+++ src/test/java/app/controller/ErrorControllerTest.java
@@ -1,31 +1,13 @@
 package app.controller;
 
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.mockito.internal.util.reflection.Whitebox;
-import org.mockito.stubbing.OngoingStubbing;
-import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.servlet.ModelAndView;
-
 import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-
-import javax.persistence.EntityNotFoundException;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.MockitoAnnotations;
+import org.springframework.ui.ModelMap;
 
-import app.base.service.CommentService;
-import app.base.service.PostService;
-import app.base.service.UserSessionManagementService;
-import app.bean.CommentForm;
-import app.bean.PostForm;
-import app.entity.Comment;
-import app.entity.Post;
-import app.entity.User;
+
 
 public class ErrorControllerTest {
   
@@ -48,15 +30,4 @@ public class ErrorControllerTest {
         assertTrue(modelMap.get("error").equals(error));
         
     }
-=======
-import static org.junit.Assert.*;
-
-import org.junit.Test;
-
-public class ErrorControllerTest {
-
-	@Test
-	public void test() {
-		
-	}
 }
